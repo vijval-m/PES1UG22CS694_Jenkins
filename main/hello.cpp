5f67f2fed53e671080929cdd33e3a01eb7610137
vijval-m <151124640+vijval-m@users.noreply.github.com>
2025-03-18 15:01:48 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Hello, PES1UG22CS694";
-  cout << "Modfied Code";
+  cout << "Hello, PES1UG22CS694\n";
+  cout << "Modfied Code\n";
   return 0;
 }
